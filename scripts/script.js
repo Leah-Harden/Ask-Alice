3c9d890cb4f0ebe97d08c4ef80bf1f3b4d1808bb
Leah <leahxharden@gmail.com>
2023-02-08 15:28:20 -0500

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 
 function search(){
 
-    fetch('https://api.fda.gov/drug/enforcement.json?search=classification:"ClassIII"&limit=5')
+    fetch('https://api.fda.gov/drug/event.json?api_key=fMBkSpWpi59SAripPdWheuRojQkyA6OF0gT5n64y&search=patient.reaction.reactionmeddrapt:"fatigue"&limit=1')
     then((response) => response.json())
     .then((data) => console.log(data));
 }
