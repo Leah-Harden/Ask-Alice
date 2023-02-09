14440383ef5ab3f8c5e63224a0e302f57a065ca7
Leah <leahxharden@gmail.com>
2023-02-08 20:13:36 -0500

    add the beginning of dates

--- scripts/script.js
+++ scripts/script.js
@@ -2,39 +2,46 @@
 
 $( function() {
     var dateFormat = "YYYYMMDD",
-      from = $( "#from" )
+    from = $( "#from" )
         .datepicker({
-          defaultDate: "+1w",
-          changeMonth: true,
-          numberOfMonths: 3
-        })
-        .on( "change", function() {
-          to.datepicker( "option", "minDate", getDate( this ) );
-        }),
-      to = $( "#to" ).datepicker({
         defaultDate: "+1w",
         changeMonth: true,
         numberOfMonths: 3
-      })
-      .on( "change", function() {
+        })
+        .on( "change", function() {
+        to.datepicker( "option", "minDate", getDate( this ) );
+        }),
+    to = $( "#to" ).datepicker({
+        defaultDate: "+1w",
+        changeMonth: true,
+        numberOfMonths: 3
+    })
+    .on( "change", function() {
         from.datepicker( "option", "maxDate", getDate( this ) );
-      });
- 
+    });
+
     function getDate( element ) {
-      var date;
-      try {
+    var date;
+    try {
         date = $.datepicker.parseDate( dateFormat, element.value );
-      } catch( error ) {
+    } catch( error ) {
         date = null;
-      }
- 
-      return date;
     }
-  } );
-  
+
+    return date;
+    }
+} );
+
+var oneDate =
+
+var twoDate =
+
+
+
+
 function search(){
 
-    fetch('https://api.fda.gov/drug/enforcement.json?search=report_date:[20040101+TO+20131231]&limit=1')
+    fetch('https://api.fda.gov/drug/enforcement.json?search=report_date:[',oneDate,'+TO+',twoDate,']&limit=1')
     .then((response) => response.json())
     .then((data) => console.log(data));
 }
