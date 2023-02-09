0c98e3523a3152dfe26bf265c7f069f11a64d590
Leah <leahxharden@gmail.com>
2023-02-08 19:47:24 -0500

    add bunny photo

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 
 function search(){
 
-    fetch('https://api.fda.gov/drug/event.json?api_key=fMBkSpWpi59SAripPdWheuRojQkyA6OF0gT5n64y&search=patient.reaction.reactionmeddrapt:"fatigue"&limit=1')
+    fetch('https://api.fda.gov/drug/enforcement.json?search=report_date:[20040101+TO+20131231]&limit=1')
     .then((response) => response.json())
     .then((data) => console.log(data));
 }
