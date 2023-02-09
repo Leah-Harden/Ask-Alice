3322d41140a9e9901fa99d76e71e58c77dec0b49
gman <dolivafigueroa@gmail.com>
2023-02-08 20:00:58 -0500

    add js for calendars

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,37 @@
 
+
+$( function() {
+    var dateFormat = "YYYYMMDD",
+      from = $( "#from" )
+        .datepicker({
+          defaultDate: "+1w",
+          changeMonth: true,
+          numberOfMonths: 3
+        })
+        .on( "change", function() {
+          to.datepicker( "option", "minDate", getDate( this ) );
+        }),
+      to = $( "#to" ).datepicker({
+        defaultDate: "+1w",
+        changeMonth: true,
+        numberOfMonths: 3
+      })
+      .on( "change", function() {
+        from.datepicker( "option", "maxDate", getDate( this ) );
+      });
+ 
+    function getDate( element ) {
+      var date;
+      try {
+        date = $.datepicker.parseDate( dateFormat, element.value );
+      } catch( error ) {
+        date = null;
+      }
+ 
+      return date;
+    }
+  } );
+  
 function search(){
 
     fetch('https://api.fda.gov/drug/event.json?api_key=fMBkSpWpi59SAripPdWheuRojQkyA6OF0gT5n64y&search=patient.reaction.reactionmeddrapt:"fatigue"&limit=1')
@@ -7,4 +40,3 @@ function search(){
 }
 
 search()
-
