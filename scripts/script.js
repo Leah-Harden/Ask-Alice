332c7690e0c68c5e37420c8420dd7cfb87581f33
andybockwrites <andybockwrites@gmail.com>
2023-02-08 19:22:30 -0500

    *correctly* linked the UIkit files we needed to our HTML

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@
 function search(){
 
     fetch('https://api.fda.gov/drug/event.json?api_key=fMBkSpWpi59SAripPdWheuRojQkyA6OF0gT5n64y&search=patient.reaction.reactionmeddrapt:"fatigue"&limit=1')
-    then((response) => response.json())
+    .then((response) => response.json())
     .then((data) => console.log(data));
 }
 
